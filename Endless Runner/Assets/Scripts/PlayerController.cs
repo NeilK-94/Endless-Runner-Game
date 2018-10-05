9a1a28fb3dbef5ebd573c0adde116c322bb29487
NeilK-94 <neil.kyne@gmail.com>
2018-10-05 19:07:50 +0100

    Player Controls

---
+++ Endless Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+        }
+	}
+}
